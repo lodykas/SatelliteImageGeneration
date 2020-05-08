5c7d9a740a6d017cc1d8b53c0768099f0464071a
unknown <lucas.ody@student.uclouvain.be>
2020-05-08 12:18:13 +0200

    better evaluation method

--- val.py
+++ val.py
@@ -13,37 +13,48 @@ num_epochs = 5
 lr = 0.0002
 beta1 = 0.5
 
+
 # custom weights initialization called on netG and netD
 def weights_init(m):
-	classname = m.__class__.__name__
-	if classname.find('Conv') != -1:
-		nn.init.normal_(m.weight.data, 0.0, 0.02)
-	elif classname.find('BatchNorm') != -1:
-		nn.init.normal_(m.weight.data, 1.0, 0.02)
-		nn.init.constant_(m.bias.data, 0)
-	
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
 
 transform = transforms.Compose(
-	[transforms.ToPILImage(),
-	 transforms.Resize([image_size,image_size]),
-	 transforms.ToTensor()])
-
+    [transforms.ToPILImage(),
+     transforms.Resize([image_size, image_size]),
+     transforms.ToTensor()])
 
 dataSet = SatelliteDataset("val\\", transform=transform, data_augment=None)
-dataloader = torch.utils.data.DataLoader(dataSet, batch_size=batch_size, shuffle=True, num_workers=0)
+dataloader = torch.utils.data.DataLoader(dataSet, batch_size=batch_size, shuffle=False, num_workers=0)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+netC = Classifier(9).to(device)
+netCparam = torch.load("models\\Classifier.nn")
+netC.load_state_dict(netCparam)
 
-# Create the generator
-netG = UnetGenerator(nc + nz, 3, 7, use_dropout=True).to(device)
-netG2 = torch.load("pix2pix\\pytorch-CycleGAN-and-pix2pix\\checkpoints\\maps_pix2pix\\10_net_G.pth")
-netG1 = torch.load("models\\Generator.nn")
-netG.load_state_dict(netG1)
+avg = 0
+res = []
+false_negatives = 0
+
 for i, data in enumerate(dataloader, 0):
-	data, mask = data
-	mask = mask.to(device)
-	noise = Tensor(torch.randn(1, nz, 1, 1)).repeat(1,1,image_size, image_size).to(device)
-	fake = netG(torch.cat((mask, noise), 1))
-	to_save = torch.cat( (fake[0], mask[0]), 2)
-	vutils.save_image(to_save, '%s/samples_epoch_%03d_mask.png' % ("./results", i))
+    data, label = data
+    data = data.to(device)
+
+    out = netC(data)[0].item()
+    res.append(out)
+    avg += out
+    if out < 0.5:
+        false_negatives += 1
+
+avg /= len(dataloader)
+print(avg)
+print("false negatives: %d out of %d images" % (false_negatives, len(dataloader)))
+res.sort()
+print(res[0], res[-1])
+
